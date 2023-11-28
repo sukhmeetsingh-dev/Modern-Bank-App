444308c004076889d6a0426f020011041e9f848a
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-11-29 00:39:35 +0530

    billing section complete

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -9,6 +9,18 @@ const Billing = () => (
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"/>
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/>
       </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden"/> billing & invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis commodi laborum voluptatibus nulla. Recusandae id placeat, dolorem eaque incidunt illum.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+          <img src={google} alt="play_store" className="w-[128px] h-[42px] object-contain cursor-pointer" />
+        </div>
+      </div>
   </section>
 )
 
