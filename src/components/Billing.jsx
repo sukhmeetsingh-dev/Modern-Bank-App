5c9b9da58f5b55a995597f248e2c76296856ac7c
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-11-28 23:50:43 +0530

    created bank app & some sections

---
+++ src/components/Billing.jsx
@@ -0,0 +1,10 @@
+import { apple, bill, google } from '../assets'
+import styles, { layout } from '../style'
+
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+
+  </section>
+)
+
+export default Billing
